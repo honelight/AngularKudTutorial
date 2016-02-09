63c5ea7cec2944d9e195596a8975e7dbe6f98c6d
Hongyao Shi <honelight@gmail.com>
2016-02-08 22:05:10 -0500

    ng-src example added

---
+++ AngularTutorial1/Scripts/ngSrcDirective.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="angular.min.js" />
+
+var ngSrcDirectiveApp = angular.module("ngSrcDirectiveModule", []);
+
+ngSrcDirectiveApp.controller("ngSrcDirectiveController", function ($scope) {
+    var country = {
+        name: "USA",
+        capital: "Washington DC",
+        flag: "image/us-flag.gif"
+    };
+
+    $scope.country = country;
+});
