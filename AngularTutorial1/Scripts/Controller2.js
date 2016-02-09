4337bcb78f236a9e78f66bdc5c2ccc0fbcca2798
Hongyao Shi <honelight@gmail.com>
2016-02-08 21:33:52 -0500

    More details on Controllers

---
+++ AngularTutorial1/Scripts/Controller2.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="angular.min.js" />
+
+var myAppController2 = angular.module("myModuleController2", []);
+
+myAppController2.controller("myControllerController2", function ($scope) {
+    var employee = {
+        firstName: 'David',
+        lastName: 'Hastings',
+        gender: 'Male'
+    };
+
+    $scope.employee = employee;
+});
