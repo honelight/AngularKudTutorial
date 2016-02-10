c2d85b5bdbcff198f16cad517a84d7d919147019
Hongyao Shi <honelight@gmail.com>
2016-02-09 21:03:03 -0500

    add in event handling example

---
+++ AngularTutorial1/Scripts/HandlingEvents.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="angular.min.js" />
+
+var handlingEventsApp = angular.module("handlingEventsModule", []);
+
+handlingEventsApp.controller("handlingEventsController", function ($scope) {
+    $scope.techonolgies = [
+        { name: "C#", likes: 0, dislikes: 0 },
+        { name: "ASP.NET", likes: 0, dislikes: 0 },
+        { name: "SQL Server", likes: 0, dislikes: 0 },
+        { name: "AngularJS", likes: 0, dislikes: 0 }
+    ];
+
+    $scope.incrementLikes = function (technology) {
+        technology.likes += 1;
+    }
+
+    $scope.incrementDislikes = function (technology) {
+        technology.dislikes += 1;
+    }
+})
