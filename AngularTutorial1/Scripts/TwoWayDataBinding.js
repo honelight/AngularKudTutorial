f7f37ec66e6666ee3c055f7b65c5e3c9fdce29f1
Hongyao Shi <honelight@gmail.com>
2016-02-09 19:46:29 -0500

    Added in examples for Two Way data binding

---
+++ AngularTutorial1/Scripts/TwoWayDataBinding.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="angular.min.js" />
+
+var twoWayDataBindingApp = angular
+                                   .module("twoWayDataBindingModule", [])
+                                   .controller("twoWayDataBindingController", function ($scope) {
+                                       $scope.message = "Hello Angular";
+                                       $scope.employee = {
+                                           firstName: "Ben",
+                                           lastName: "Hastings",
+                                           gender: "Male"
+                                       };
+                                   });
