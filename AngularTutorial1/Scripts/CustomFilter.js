3b01567652a3659d6093671b12b49917bd5b48fe
Hongyao Shi <honelight@gmail.com>
2016-02-13 21:09:12 -0500

    add the custom filter example

---
+++ AngularTutorial1/Scripts/CustomFilter.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="angular.min.js" />
+
+var customFilterModule = angular.module("customFilterModule", []);
+
+
+customFilterModule.controller("customFilterController", function ($scope) {
+    var employees = [
+        { name: "Ben", gender: 1, city: "London" },
+        { name: "Sara", gender: 2, city: "Los Angles" },
+        { name: "Mark", gender: 1, city: "New York" },
+        { name: "Pam", gender: 2, city: "San Francisco" },
+        { name: "Todd", gender: 3, city: "Hong Kong" }
+    ];
+
+    $scope.employees = employees;
+    
+})
+
---
+++ AngularTutorial1/Scripts/Filters/GenderFilter.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../angular.min.js" />
+/// <reference path="../CustomFilter.js" />
+
+
+customFilterModule.filter("gender", function () {
+    return function (gender) {
+        switch (gender) {
+            case 1: {
+                return "Male";
+            }
+            case 2: {
+                return "Female";
+            }
+            case 3: {
+                return "Not disclosed";
+            }
+        }
+    }
+});
