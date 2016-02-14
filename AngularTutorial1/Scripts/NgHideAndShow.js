a62cb084db831410026601fd0df58e3fa960052c
Hongyao Shi <honelight@gmail.com>
2016-02-13 21:26:01 -0500

    added in ng-hide and ng-show example

---
+++ AngularTutorial1/Scripts/NgHideAndShow.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="angular.min.js" />
+
+
+var NgHideAndShowModule = angular.module("NgHideAndShowModule", []);
+
+NgHideAndShowModule.controller("NgHideAndShowController", function ($scope) {
+    var employees = [
+        { name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", city: "London" },
+        { name: "Sara", dateOfBirth: new Date("May 05, 1970"), gender: "Female", city: "Los Angles" },
+        { name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender: "Male", city: "New York" },
+        { name: "Pam", dateOfBirth: new Date("October 27, 1979"), gender: "Female", city: "San Francisco" },
+        { name: "Todd", dateOfBirth: new Date("December 30, 1983"), gender: "Male", city: "Hong Kong" }
+    ];
+
+    $scope.employees = employees;
+})
