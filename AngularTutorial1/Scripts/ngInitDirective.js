0d42c15c4d4872ac715d05e21cdefe9d9e8a379a
Hongyao Shi <honelight@gmail.com>
2016-02-15 20:40:07 -0500

    ng-init example added

---
+++ AngularTutorial1/Scripts/ngInitDirective.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="angular.min.js" />
+
+var ngInitDirectiveModule = angular.module("ngInitDirectiveModule", []);
+ngInitDirectiveModule.controller("ngInitDirectiveController", function ($scope) {
+    employees = [
+            { name: 'Ben', gender: 'Male', salary: '55000.788' },
+            { name: 'Sara', gender: 'Female', salary: '68000' },
+            { name: 'Mark', gender: 'Male', salary: '57000' },
+            { name: 'Pam', gender: 'Female', salary: '53000' },
+            { name: 'Todd', gender: 'Male', salary: '60000' }
+    ]
+
+    $scope.employees = employees
+});
